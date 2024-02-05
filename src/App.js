d80eff459db62ab9d1a6d15d1cd0c31b60c0a4ff
Pardeshi2309 <pardeshijaiswar.149@gmail.com>
2024-02-05 23:05:00 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import image from './image.jpeg';
 function App() {
   return (
-    <div className="flex items-center justify-center mt-10 ">
-      <div className="bg-[#1E1E1E] flex items-center justify-center w-[400px] h-[550px] text-white rounded-2xl flex-col border border-red-500">
-        <img className="rounded-full object-contain h-30 w-20" src={image} alt="img" />
-        <p className="font-bold">Jessica Randall</p>
-        <span className="text-[#C4F82A] font-bold">London,United Kingdom</span>
-        <p className="mt-5">"Front-end developer and Avid reader."</p>
-
-
-      </div>
+    <div className="flex items-center justify-center">
+      <div className=" bg-[#1E1E1E] flex items-center justify-center w-[400px] h-screen text-white rounded-2xl flex-col border border-red-500">
+        <img className="absolute top-10 rounded-full object-contain h-30 w-20" src={image} alt="img" />
+        <p className="font-bold absolute top-20 mt-20 text-[20px]">Jessica Randall</p>
+        <span className="text-[#C4F82A] font-bold absolute top-[24%] mt-10">London,United Kingdom</span>
+        </div>
+        <div className="flex items-center justify-center absolute top-[38%]">
+        <span className="text-white">"Front-end developer and avid reader."</span>
+        </div>
 
 
     </div>
