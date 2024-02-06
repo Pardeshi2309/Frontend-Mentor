604c8edcfc96b5c1d6aef76b9c928b2214001d92
Pardeshi2309 <pardeshijaiswar.149@gmail.com>
2024-02-06 22:05:57 +0530

    added src

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import image from './image.jpeg';
 function App() {
   return (
     <div className="flex items-center justify-center">
-      <div className=" bg-[#1E1E1E] flex items-center justify-center w-[400px] h-screen text-white rounded-2xl flex-col border border-red-500">
-        <img className="absolute top-10 rounded-full object-contain h-30 w-20" src={image} alt="img" />
-        <p className="font-bold absolute top-20 mt-20 text-[20px]">Jessica Randall</p>
-        <span className="text-[#C4F82A] font-bold absolute top-[24%] mt-10">London,United Kingdom</span>
+      <div className=" bg-[#1E1E1E] flex items-center justify-start p-10 w-[400px] h-screen text-white rounded-2xl flex-col border">
+        <img className="rounded-full object-contain h-30 w-20" src={image} alt="img" />
+        <h2 className="font-bold text-[20px] mt-10">Jessica Randall</h2>
+        <h3 className="text-[#C4F82A] mt-2 font-bold">London,United Kingdom</h3>
+        <span className="text-white py-5">"Front-end developer and avid reader."</span>
+        <div className="flex items-center justify-center flex-col gap-5 w-full">
+          <a className='text-white text-center w-full py-2 bg-[#333333] rounded-md' href='GitHub'>GitHub</a>
+          <a className='text-white text-center w-full py-2 bg-[#333333] rounded-md' href='GitHub'>Frontend Mentor</a>
+          <a className='text-white text-center w-full py-2 bg-[#333333] rounded-md' href='GitHub'>LinkedIn</a>
+          <a className='text-white text-center w-full py-2 bg-[#333333] rounded-md' href='GitHub'>Twitter</a>
+          <a className='text-white text-center w-full py-2 bg-[#333333] rounded-md' href='GitHub'>Instagram</a>
         </div>
-        <div className="flex items-center justify-center absolute top-[38%]">
-        <span className="text-white">"Front-end developer and avid reader."</span>
-        </div>
-
-
+      </div>
     </div>
+
   );
 }
 
